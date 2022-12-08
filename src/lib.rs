8dfb79d1bd988c9e50f6e8fc20e82fb2e2159d5b
李铭辉 <1018596741@qq.com>
2022-12-08 23:07:00 +0800

    chore: 新增swc依赖

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![deny(clippy::all)]
+
+use std::collections::HashMap;
 mod utils;
 #[macro_use]
 extern crate napi_derive;
@@ -8,10 +10,14 @@ pub fn sum(a: i32, b: i32) -> i32 {
   a + b
 }
 
-use napi::JsObject;
+#[napi(object)]
+pub struct ConfigObject {
+  pub path: Option<String>,
+  pub alias: Option<HashMap<String,String>>
+}
 
 #[napi]
-pub fn init(config: JsObject) -> Option<String> {
-  let entry:Option<String> = config.get("path").expect("msg");
+pub fn init(config: ConfigObject) -> Option<String> {
+  let entry:Option<String> = config.path;
   entry
 }
