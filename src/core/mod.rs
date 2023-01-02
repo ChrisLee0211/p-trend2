572603fae010a401b1adb2066a8170a90244053f
李铭辉 <1018596741@qq.com>
2023-01-02 18:12:10 +0800

    feat: 新增Stack结构工具

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,20 +1,26 @@
 use std::collections::HashMap;
 use std::{fs, io::Error, env};
 
+#[derive(Debug)]
 pub struct FileNode {
     file_path: String,
     file_name: String,
     is_folder:bool,
-    deps:Vec<DependenceNode>,
+    deps:Vec<String>,
     parent: Box<FileNode>,
     children:Vec<Box<FileNode>>
 }
 
-pub struct DependenceNode {
-    dep_path:String,
+
+pub struct FileNodeForHash {
+    // 节点指向
+    node: Box<FileNode>,
+    // 被引用的节点path
+    referencePath: Vec<String>
 }
 
 pub fn scan_by_entry(entry: String, alias_config:HashMap<String, String>, excludes:Vec<String>) -> Result<(), Error> {
+    let file_hash_map:HashMap<String, FileNodeForHash> = HashMap::new();
     for file in fs::read_dir(entry)? {
         let file = file?;
         let path = file.path();
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,3 +7,43 @@ use std::io::{Error};
 pub fn read_file_to_string(path:&String) -> Result<String,Error> {
     fs::read_to_string(path)
 }
+
+
+pub struct Stack <T>{
+    top: Option<Box<StackNode<T>>>
+}
+
+pub struct  StackNode<T> {
+    value: T,
+    next: Option<Box<StackNode<T>>>
+}
+
+impl<T> StackNode<T> {
+    fn new(value:T) -> StackNode<T> {
+        StackNode{value, next: None}
+    }
+}
+
+impl<T> Stack<T> {
+    fn new() -> Stack<T> {
+        Stack { top: None }
+    }
+
+    fn push(&mut self,val:T) {
+        let mut node = StackNode::new(val);
+        let next = self.top.take();
+        node.next = next;
+        self.top = Some(Box::new(node));
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        let result = self.top.take();
+        match result {
+            Some(mut v) => {
+                self.top = v.next.take();
+                Some(v.value)
+            },
+            None => None
+        }
+    }
+}
